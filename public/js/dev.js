be0a9438f6bbb110151619b726c98b93192f2d03
jamesbaroi <james.a.baroi@gmail.com>
2023-07-04 21:59:50 -0700

    update

--- public/js/dev.js
+++ public/js/dev.js
@@ -1,35 +1,23 @@
 const siteUnderDev = () => {
 
   let dev = document.createElement('div')
-  let cnt = document.createElement('div')
-
-  let ck = document.getElementById('ck')
-  let plc = document.getElementById('plc')
-
-  if (ck) {
-
-    ck.style.display = 'none'
-  }
-
-  if (plc) {
-
-    plc.style.display = 'none'
-  }
 
   dev.innerHTML = 'Site under development.'
-  cnt.innerHTML = 'Contact: james.a.baroi@gmail.com'
 
   document.querySelector('header').remove()
   document.querySelector('main').remove()
   document.querySelector('footer').remove()
 
-  document.body.style.backgroundColor = 'rgb(32, 33, 36)'
-  document.body.style.margin = '16px auto'
-  document.body.style.padding = '16px'
-  document.body.style.textAlign = 'center'
+  document.body.style.cssText = `
+    background-color: rgb(32, 33, 36);
+    color: rgb(140, 143, 150);
+    font-family: monospace;
+    margin: 16px auto;
+    padding: 16px;
+    text-align: center;
+  `
 
   document.body.append(dev)
-  document.body.append(cnt)
 
   console.log('Site under developement.')
 }
