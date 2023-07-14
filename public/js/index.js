e2c9845369c50760fcde8a743b6c3c7da14d82bc
jamesbaroi <james.a.baroi@gmail.com>
2023-07-13 19:28:39 -0700

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,1 +1,17 @@
-document.body.innerHTML = 'Site under development.'
+const siteUnderDev = () => {
+  document.body.innerHTML = 'Site under development.'
+  document.body.style = `
+    align-items: center;
+    background-color: black;
+    color: gray;
+    display: grid;
+    font-family: monospace;
+    font-size: 16px;
+    height: 100vh;
+    margin: 0;
+    text-size-adjust: none;
+    text-align: center;
+  `
+}
+
+siteUnderDev()
