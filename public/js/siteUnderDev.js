96eec3d59f33cdb99757d247d5b23c5aa2703203
jamesbaroi <james.a.baroi@gmail.com>
2023-07-13 23:23:59 -0700

    update

---
+++ public/js/siteUnderDev.js
@@ -0,0 +1,14 @@
+/** https://jamesbaroi.github.io/public/js/siteUnderDev.js */
+
+const siteUnderDev = () => {
+  document.body.innerHTML = 'Site under development.'
+  document.body.style = `
+    background-color: rgb(16, 16.5, 18);
+    color: rgb(140, 143, 150);
+    font-family: monospace;
+    font-size: 16px;
+    margin-top: 33vh;
+    text-size-adjust: none;
+    text-align: center;
+  `
+}
