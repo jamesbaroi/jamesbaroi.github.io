5752eff245b7820710d07852523a5d2a259634b4
jamesbaroi <james.a.baroi@gmail.com>
2023-07-13 21:03:26 -0700

    update

--- public/js/index.js
+++ public/js/index.js
@@ -1,14 +1,10 @@
 const siteUnderDev = () => {
   document.body.innerHTML = 'Site under development.'
   document.body.style = `
-    align-items: center;
     background-color: rgb(16, 16.5, 18);
     color: rgb(140, 143, 150);
-    display: grid;
     font-family: monospace;
-    font-size: 16px;
-    height: 100vh;
-    margin: 0;
+    margin-top: 33vh;
     text-size-adjust: none;
     text-align: center;
   `
