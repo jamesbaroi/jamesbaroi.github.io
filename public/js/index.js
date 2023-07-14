9cb2f20357811a4735b91e4d04621dcb0faa8fdb
jamesbaroi <james.a.baroi@gmail.com>
2023-07-13 21:16:17 -0700

    update

--- public/js/index.js
+++ public/js/index.js
@@ -4,6 +4,7 @@ const siteUnderDev = () => {
     background-color: rgb(16, 16.5, 18);
     color: rgb(140, 143, 150);
     font-family: monospace;
+    font-size: 16px;
     margin-top: 33vh;
     text-size-adjust: none;
     text-align: center;
