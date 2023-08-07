5351ff634f94fc07c10f9fdd7b12d3fd9a4cf458
jamesbaroi <james.a.baroi@gmail.com>
2023-08-07 05:11:59 -0700

    update

--- public/js/index.js
+++ public/js/index.js
@@ -37,61 +37,76 @@ const showBlog = () => {
 
 const showAbout = () => {
   about.style.display                  = 'block'
-  apps.style.display                 = 'none'
+  apps.style.display                   = 'none'
   blog.style.display                   = 'none'
-  posts_Blog.style.display                  = 'none'
-  posts_App.style.display             = 'none'
+  posts_Blog.style.display             = 'none'
+  posts_App.style.display              = 'none'
   !btn_Blog_Posts ? true :
-  btn_Blog_Posts.style.display              = 'none'
+  btn_Blog_Posts.style.display         = 'none'
   !btn_App_Posts ? true :
-  btn_App_Posts.style.display              = 'none'
+  btn_App_Posts.style.display          = 'none'
   btn_About.style.display              = 'none'
   btn_Blog.style.display               = 'block'
-  btn_Apps.style.display             = 'block'
+  btn_Apps.style.display               = 'block'
 }
 
 const showApps = () => {
-  apps.style.display                 = 'block'
+  apps.style.display                   = 'block'
   about.style.display                  = 'none'
   blog.style.display                   = 'none'
-  posts_Blog.style.display                  = 'none'
-  posts_App.style.display             = 'none'
+  posts_Blog.style.display             = 'none'
+  posts_App.style.display              = 'none'
   !btn_Blog_Posts ? true :
-  btn_Blog_Posts.style.display              = 'none'
+  btn_Blog_Posts.style.display         = 'none'
   !btn_App_Posts ? true :
-  btn_App_Posts.style.display              = 'block'
-  btn_Apps.style.display             = 'none'
+  btn_App_Posts.style.display          = 'block'
+  btn_Apps.style.display               = 'none'
   btn_Blog.style.display               = 'block'
   btn_About.style.display              = 'block'
 }
 
 const showBlogPosts = () => {
-  posts_Blog.style.display                 != 'none' ? (
-    posts_Blog.style.display                = 'none',
+  posts_Blog.style.display            != 'none' ? (
+    posts_Blog.style.display           = 'none',
     document.body.scrollTop            = 0,
     document.documentElement.scrollTop = 0
   ) : (
-    posts_Blog.style.display                = 'block',
+    posts_Blog.style.display           = 'block',
     blog.scrollIntoView()
   )
 }
 
 const showAppPosts = () => {
-  posts_App.style.display                 != 'none' ? (
-    posts_App.style.display                = 'none',
+  posts_App.style.display             != 'none' ? (
+    posts_App.style.display            = 'none',
     document.body.scrollTop            = 0,
     document.documentElement.scrollTop = 0
   ) : (
-    posts_App.style.display                = 'block',
+    posts_App.style.display            = 'block',
     apps.scrollIntoView()
   )
 }
 
 const onPageLoad = () => {
 
-  if      (url.hash == '#about' ) showAbout()
-  else if (url.hash == '#blog')   showBlog()
-  else                            showApps()
+  if (url.hash == '#about' ) {
+
+    showAbout()
+
+  } else if (url.hash == '#blog') {
+
+    showBlog()
+    showBlogPosts()
+
+  } else if (url.hash == '#app') {
+
+    showApps()
+    showAppPosts()
+
+  } else {
+
+    showApps()
+  }
 
   history.pushState(
     "",
